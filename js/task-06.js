bc0571042ca8ea7378178b0566b2ddd22a7ecf87
Kathryne-Kov <112563496+Kathryne-Kat@users.noreply.github.com>
2022-12-23 19:48:45 +0200

    add #9

--- js/task-06.js
+++ js/task-06.js
@@ -1,12 +1,12 @@
 const userInput = document.querySelector('#validation-input');
 
 const userInputBlur = () => {    
-    console.log(userInput.value.length);
+    //console.log(userInput.value.length);
     if (Number(userInput.getAttribute('data-length')) === userInput.value.length) {
-        console.log('true'); 
+        //console.log('true'); 
         userInput.classList.add('valid');                   
     } else if (Number(userInput.getAttribute('data-length')) !== userInput.value.length) {
-        console.log('false');
+        //console.log('false');
         userInput.classList.add('invalid');   
     }
 };
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const colorBody = document.querySelector('body');
+const colorName = document.querySelector('.color');
+const btnChangeColor = document.querySelector('.change-color');
+
+const colorBodyEl = () => {
+  colorBody.style.backgroundColor = getRandomHexColor();
+  colorName.textContent = getRandomHexColor();
+};
+
+btnChangeColor.addEventListener('click', colorBodyEl)
+
