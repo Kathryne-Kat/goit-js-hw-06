0276afcf2893a7be3c3edae9b524410a7dee7b1e
Kathryne-Kov <112563496+Kathryne-Kat@users.noreply.github.com>
2022-12-25 15:57:52 +0200

    add #10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,32 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const collectionInput = document.querySelector('input');
+const collectionBtnCreate = document.querySelector('button');
+const collectionBtnDestroy = document.querySelector('button+button');
+const collectionEl = document.querySelector('#boxes');
+
+function createBoxes(amount) {
+  amount = collectionInput.valueAsNumber;
+  for (let i = 1; i <= amount; i += 1){
+    let itemEl = document.createElement('div');
+    itemEl.classList.add('item');
+    let widthEl = 20 + 10 * i;
+    let heightEl = 20 + 10 * i;
+    itemEl.style.width = ''+ widthEl+'px';
+    itemEl.style.height = '' + heightEl + 'px';
+    itemEl.style.backgroundColor = getRandomHexColor();
+    collectionEl.prepend(itemEl);
+  }   
+} 
+
+const destroyBoxes = () => {
+  let delEl = document.querySelectorAll('.item');
+  delEl.forEach((el) => {
+    el.remove();
+  })
+}
+
+collectionBtnCreate.addEventListener("click", createBoxes);
+collectionBtnDestroy.addEventListener("click", destroyBoxes);
